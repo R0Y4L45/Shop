9ad903341397bcbe05d616ece512ad7ff7830a11
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2022-11-24 21:33:21 +0400

    Initial commit message

---
+++ ShopWpf/FakeDatas/FakeProducts.cs
@@ -0,0 +1,27 @@
+﻿using ShopProgramWpf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProgramWpf.FakeDatas;
+
+public class FakeProducts
+{
+    public List<Product> products = new()
+    {
+        new Product("C:\\Users\\Администратор\\Source\\Repos\\ShopWpfs\\ShopWpf\\Images\\iphone_11.jpg","Iphone",12,12.5f),
+        new Product("","Samsung",34,1.5f),
+        new Product("","Xiaomi",12,2.5f),
+        new Product("","Samsung",34,1.5f),
+        new Product("","Xiaomi",12,2.5f),
+         new Product("","Samsung",34,1.5f),
+        new Product("","Xiaomi",12,2.5f),
+        new Product("","Samsung",34,1.5f),
+        new Product("","Xiaomi",12,2.5f),
+
+        new Product("C:\\Users\\Администратор\\Source\\Repos\\ShopWpfs\\ShopWpf\\Images\\iphone_11.jpg","Iphone",12,12.5f),
+    };
+}
---
+++ ShopWpf/UserControls/Uc_Control.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ShopProgramWpf.Models;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ShopProgramWpf.UserControls
+{
+    public partial class Uc_Control : UserControl, INotifyPropertyChanged
+    {
+        public Product? product { get; set; }
+        public int count { get; set; }
+        public float cost { get; set; }
+        public bool isAdd { get; set; }
+        public float Sum { get; set; }
+
+
+
+        public Uc_Control(int count,float cost)
+        {
+            InitializeComponent();
+            DataContext = this;
+            this.count = count;
+            this.cost = cost;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (product!.Count == 0)
+                return;
+            isAdd = true;
+            product.Count--;
+            Sum += product.Cost;
+            lblConutProduct.Content = product.Count.ToString();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (product!.Count == count)
+                return;
+            isAdd = false;
+            product.Count++;
+            Sum -= product.Cost;
+            lblConutProduct.Content = product.Count.ToString();
+        }
+
+        
+    }
+}
